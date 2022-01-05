dd57835f05550f56cd42349c7829c73ea82322d6
Jess Mark Sevilleja <jess@clinica.dev>
2022-01-05 14:15:42 +0800

    added sso login logout

---
+++ src/components/Chat/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './chat.css';
+import {useNavigate} from "react-router-dom";
+import {useState, useEffect} from "react";
+
+function eraseCookie(name) {
+    document.cookie = name + "=; Max-Age=-99999999;";
+}
+
+const Chat = () => {
+
+    let navigate = useNavigate();
+
+    const __handleLogout = async () => {
+        localStorage.removeItem("user_id");
+        localStorage.removeItem("auth_login");
+        localStorage.removeItem("auth_token");
+        localStorage.removeItem("refresh_token");
+        localStorage.removeItem("access_token");
+        eraseCookie("auth_login");
+        eraseCookie("token");
+        navigate(`/`);
+    };
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+            <div >
+                CHAT HERE
+            </div>
+            <div>
+                <button onClick={__handleLogout}>Logout</button>
+            </div>
+
+        </>
+    )
+}
+
+export default Chat
---
+++ src/components/Login/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './login.css';
+import {Link} from 'react-router-dom'
+
+const Login = () => {
+    return (
+        <>
+            <div >
+                LOGO HERE
+            </div>
+
+            <div >
+                <Link to="/login"><button>Login</button></Link>
+            </div>
+        </>
+    )
+}
+
+export default Login
