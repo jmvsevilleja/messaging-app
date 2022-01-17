b21dfde2085fcb4968e875de1230c9e94159c691
Jess Mark Sevilleja <jess@clinica.dev>
2022-01-17 10:49:33 +0800

    added roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0084A1",
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+      fontSize: {
+        xs: ["0.75rem", { lineHeight: "1.5" }],
+        sm: ["0.875rem", { lineHeight: "1.5715" }],
+        base: ["1rem", { lineHeight: "1.5", letterSpacing: "-0.01em" }],
+        lg: ["1.125rem", { lineHeight: "1.5", letterSpacing: "-0.01em" }],
+        xl: ["1.25rem", { lineHeight: "1.5", letterSpacing: "-0.01em" }],
+        "2xl": ["1.5rem", { lineHeight: "1.33", letterSpacing: "-0.01em" }],
+        "3xl": ["1.88rem", { lineHeight: "1.33", letterSpacing: "-0.01em" }],
+        "4xl": ["2.25rem", { lineHeight: "1.25", letterSpacing: "-0.02em" }],
+        "5xl": ["3rem", { lineHeight: "1.25", letterSpacing: "-0.02em" }],
+        "6xl": ["3.75rem", { lineHeight: "1.2", letterSpacing: "-0.02em" }],
+      },
+      screens: {
+        xs: "480px",
+      },
+    },
   },
   plugins: [],
-}
+};
