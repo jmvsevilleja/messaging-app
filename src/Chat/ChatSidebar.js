4f8ec5cb349e6908a1be5d5b95bb09295dbd779b
Jess Mark Sevilleja <jess@clinica.dev>
2022-02-02 09:36:42 +0800

    added content loader

--- src/Chat/ChatSidebar.js
+++ src/Chat/ChatSidebar.js
@@ -7,6 +7,8 @@ import CreateRoom from "./CreateRoom";
 import InviteUser from "./InviteUser";
 
 import ChatProfile from "./ChatProfile";
+import AvatarWithText from "./loader/AvatarWithText";
+import ListingWithThumbnail from "./loader/ListingWithThumbnail";
 
 function ChatSidebar({
     user,
@@ -65,117 +67,122 @@ function ChatSidebar({
             id="messages-sidebar"
             className={" bg-white absolute z-20 pl-5 top-0 bottom-0 w-full md:w-auto md:static md:top-auto md:bottom-auto -mr-px md:translate-x-0 transform transition-transform duration-200 ease-in-out"
                 + (!openChat ? " translate-x-0" : " -translate-x-full")}
-
         >
+
+            {!user && <div className="my-3 pr-5">
+                <AvatarWithText height={80} width={200} className="border-b-2 border-gray-200" />
+            </div>}
             {user && <ChatProfile
                 user={user}
                 openProfile={openProfile}
                 handleCloseProfile={handleCloseProfile} />}
-            <div className="my-3 pr-5">
-                <div className="flex relative justify-between item-center p-3 px-0 mb-6 pb-0 ">
-                    <div
-                        className="grow flex"
+            {user &&
+                < div className="my-3 pr-5">
+                    <div className="flex relative justify-between item-center p-3 px-0 mb-6 pb-0 ">
+                        <div
+                            className="grow flex"
 
-                    >   <div className="flex cursor-pointer items-center" onMouseDown={() => {
-                        setDropdown(true);
-                    }}
-                        ref={dropdownMenu}>
-                            {user && (
-                                <div className="relative">
-                                    <Avatar
-                                        size="40"
-                                        round={true}
-                                        name={user.name}
-                                    />
-                                    <div className={"absolute bottom-0 right-1 w-3 h-3 bg-green-500 border-2 border-white rounded-full"}></div>
-                                </div>
-                            )}
+                        >   <div className="flex cursor-pointer items-center" onMouseDown={() => {
+                            setDropdown(true);
+                        }}
+                            ref={dropdownMenu}>
+                                {user && (
+                                    <div className="relative">
+                                        <Avatar
+                                            size="40"
+                                            round={true}
+                                            name={user.name}
+                                        />
+                                        <div className={"absolute bottom-0 right-1 w-3 h-3 bg-green-500 border-2 border-white rounded-full"}></div>
+                                    </div>
+                                )}
 
-                            <span className="block ml-2 font-bold text-base text-gray-600">
-                                {user && user.name}
-                            </span>
-                            <svg className="w-3 h-3 shrink-0 ml-1 mb-1 fill-current text-gray-400" viewBox="0 0 12 12">
-                                <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
-                            </svg>
+                                <span className="block ml-2 font-bold text-base text-gray-600">
+                                    {user && user.name}
+                                </span>
+                                <svg className="w-3 h-3 shrink-0 ml-1 mb-1 fill-current text-gray-400" viewBox="0 0 12 12">
+                                    <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
+                                </svg>
 
-                            {dropdown &&
-                                <div
-                                    className="origin-top-right z-50 absolute top-full left-0 bg-white border border-gray-200 p-1.5  rounded shadow-lg overflow-hidden mt-1 w-full md:w-1/2"
-                                >
-                                    <ul>
-                                        <li>
-                                            <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
-                                                onClick={() => {
-                                                    setOpenProfile(true);
-                                                    setDropdown(false);
-                                                }}
-                                            >Profile</button>
-                                        </li>
-                                        <li>
-                                            <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
-                                                onClick={() => {
-                                                    setOpenSetting(true);
-                                                    setDropdown(false);
-                                                }}
-                                            >Settings</button>
-                                        </li>
-                                        <li>
-                                            <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
-                                                onClick={handleLogout}
-                                            >Sign Out</button>
-                                        </li>
-                                    </ul>
-                                </div>}
+                                {dropdown &&
+                                    <div
+                                        className="origin-top-right z-50 absolute top-full left-0 bg-white border border-gray-200 p-1.5  rounded shadow-lg overflow-hidden mt-1 w-full md:w-1/2"
+                                    >
+                                        <ul>
+                                            <li>
+                                                <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
+                                                    onClick={() => {
+                                                        setOpenProfile(true);
+                                                        setDropdown(false);
+                                                    }}
+                                                >Profile</button>
+                                            </li>
+                                            <li>
+                                                <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
+                                                    onClick={() => {
+                                                        setOpenSetting(true);
+                                                        setDropdown(false);
+                                                    }}
+                                                >Settings</button>
+                                            </li>
+                                            <li>
+                                                <button className="w-full font-medium text-sm text-gray-600 hover:bg-gray-100 px-3 py-1.5 text-left"
+                                                    onClick={handleLogout}
+                                                >Sign Out</button>
+                                            </li>
+                                        </ul>
+                                    </div>}
 
+                            </div>
+
+                        </div>
+                        <div className="flex items-center">
+                            {user && < InviteUser
+                                user={user}
+                                handleChatRoomID={handleChatRoomID}
+                            />}
+                        </div>
+
+                        <div className="flex items-center">
+                            {user && < CreateRoom
+                                user={user}
+                                userList={userList}
+                                handleChatRoomID={handleChatRoomID}
+                            />}
                         </div>
 
                     </div>
-                    <div className="flex items-center">
-                        {user && < InviteUser
-                            user={user}
-                            handleChatRoomID={handleChatRoomID}
-                        />}
+                    <div className="relative text-gray-600 focus-within:text-gray-400">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <svg
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                className="w-6 h-6 text-gray-500"
+                            >
+                                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </span>
+                        <input
+                            aria-placeholder="Search"
+                            placeholder="Search"
+                            className="py-2 pl-10 pr-2 block w-full rounded bg-gray-100 border-none outline-0 focus:text-gray-700"
+                            type="search"
+                            name="search"
+                            required
+                            autoComplete="off"
+                            onChange={(e) => {
+                                setSearchText(e.target.value);
+                            }}
+                        />
                     </div>
-
-                    <div className="flex items-center">
-                        {user && < CreateRoom
-                            user={user}
-                            userList={userList}
-                            handleChatRoomID={handleChatRoomID}
-                        />}
-                    </div>
-
-                </div>
-                <div className="relative text-gray-600 focus-within:text-gray-400">
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                        <svg
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            viewBox="0 0 24 24"
-                            className="w-6 h-6 text-gray-500"
-                        >
-                            <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                        </svg>
-                    </span>
-                    <input
-                        aria-placeholder="Search"
-                        placeholder="Search"
-                        className="py-2 pl-10 pr-2 block w-full rounded bg-gray-100 border-none outline-0 focus:text-gray-700"
-                        type="search"
-                        name="search"
-                        required
-                        autoComplete="off"
-                        onChange={(e) => {
-                            setSearchText(e.target.value);
-                        }}
-                    />
-                </div>
-            </div>
+                </div>}
 
             <div className="scrollable pr-5 overflow-x-hidden overflow-y-auto shrink-0 md:w-80 xl:w-96 h-[calc(100vh-130px)]">
+                {!user && <ListingWithThumbnail height={300} width={200} />}
                 <ul>
                     {user && searchChatRoomList.length !== 0 &&
                         searchChatRoomList
