330ea1aee530f63ce422ab96906ca97ce3b86ffc
Jess Mark Sevilleja <jess@clinica.dev>
2022-02-21 13:46:54 +0800

    removed notification in nectus

--- src/Chat/ChatInfo.js
+++ src/Chat/ChatInfo.js
@@ -127,7 +127,7 @@ function ChatInfo({
                 </span>}
             </div>
             <div className="flex flex-col py-5 mx-4">
-                <div className="flex w-full p-2 text-gray-400 hover:text-gray-500">
+                {!nectus && <div className="flex w-full p-2 text-gray-400 hover:text-gray-500">
                     <div className="flex w-full">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
@@ -159,7 +159,7 @@ function ChatInfo({
                             className="outline-none focus:outline-none toggle-label block h-7 -ml-1 -mt-1 rounded-full bg-gray-400 cursor-pointer"
                         ></label>
                     </div>
-                </div>
+                </div>}
                 <div className="px-2">
                     <div className="flex text-gray-400 hover:text-gray-500">
                         <button className="py-2 flex w-full justify-between items-center outline-none focus:outline-none"
