9ffc3ea6d51f73dd21e346cd13b678d808f87d37
Jess Mark Sevilleja <jess@clinica.dev>
2022-03-02 11:45:17 +0800

    added disclaimer terms privacy pages

---
+++ src/Pages/PrivacyPolicy.js
@@ -0,0 +1,145 @@
+import ConvoLogo from '../logo.svg';
+
+export default function page() {
+    return <div>
+        <div className="w-full flex justify-between px-4 py-2 border-b border-gray-100">
+            <div className="pl-2 md:pl-4">
+                <img className="h-14 w-auto" src={ConvoLogo} />
+            </div>
+        </div>
+        <div className="flex justify-center bg-primary py-20">
+            <h1 className="font-normal text-xl md:text-5xl text-white">Private Policy</h1>
+        </div>
+        <div className="mx-4 md:mx-32 my-10">
+            <h5 className="uppercase font-semibold text-gray-700">
+                YOUR PRIVACY
+            </h5>
+            <br />
+            <p>
+                Last updated: 2 March 2022
+            </p>
+            <br />
+            <p>
+                At Conva-messenger.com, we are committed to protecting your privacy as a customer and an online website visitor. We use collected information about you to maximise our available services. We respect the privacy and confidentiality of the information provided by you and adhere to the Australian Privacy Principles. Please carefully read our privacy policy below.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                INFORMATION WE COLLECT FROM YOU
+            </h5>
+            <br />
+            <p>
+                In the course of your websites visits or use of our products and services, we may obtain the following information about you: name, company name, email address, telephone number, credit card details, billing address, geographic location, IP address, survey responses, support queries, blog comments and social media handles (together ‘Personal Data’).
+
+            </p>
+            <br />
+            <p>
+                Our services are not directed to persons under 18 and we do not knowingly collect Personal Data from anyone under 18. If we become aware that a minor under 18 has provided us with Personal Data, we will delete that information as quickly as possible. If you are the parent or guardian of a child and you believe they have provided us with Personal Data without your consent, please contact us.
+            </p>
+            <br />
+            <p>
+                You can review, correct, update or delete your Personal Data by either logging into your account and making the changes yourself or contacting us directly to do so.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                HOW WE USE YOUR INFORMATION
+            </h5>
+            <br />
+            <p>
+                Personally Identifiable Information: We use collected information to deliver our services to you, including: communicating with you, providing technical support, notifying you of updates and offers, sharing useful content, measuring customer satisfaction, diagnosing problems, and providing a personalised website experience.
+            </p>
+            <br />
+            <p>
+                Marketing communications are only sent to you if you have requested or subscribed to them. You can opt out of our marketing communications at any time by unsubscribing or emailing us and your request will be acted on immediately.
+            </p>
+            <br />
+            <p>
+                Non-Personally Identifiable Information: We also use collected information in aggregated and anonymized forms to improve our services, including: administering our website, producing reports and analytics, advertising our products and services, identifying user demands, and assisting in meeting customer needs.
+            </p>
+            <br />
+            <p>
+                Any information you choose to make publicly available, such as blog comments and testimonials on our website, will be available for others to see. If you subsequently remove this information, copies may remain viewable in cached and archived pages on other websites or if others have copied or saved the information.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                STORAGE AND SECURITY OF YOUR INFORMATION
+            </h5>
+            <br />
+            <p>
+                We will use all reasonable means to protect the confidentiality of your Personal Data while in our possession or control. All information we receive from you is stored and protected on our secure servers to prevent unauthorized use or access. Credit card information is encrypted before transmission and is not stored by us on our servers.
+            </p>
+            <br />
+            <p>
+                To be able to deliver our services, we may transfer information that we collect about you, including Personal Data, across borders for storage and processing in countries other than Australia. If your Personal Data is transferred and processed outside Australia, it will only be transferred to countries that have adequate privacy protections.
+            </p>
+            <br />
+            <p>
+                We retain your personal information for as long as needed to provide services to you and to comply with our legal obligations, resolve disputes, and enforce our agreements.
+            </p>
+            <br />
+            <p>
+                In the event of a breach of our security and your Personal Data is compromised, we will promptly notify you in compliance with the applicable law.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                COOKIES AND PIXELS
+            </h5>
+            <br />
+            <p>
+                A cookie is a small file placed in your web browser that collects information about your web browsing behaviour. Use of cookies allows a website to tailor its configurations to your needs and preferences. Cookies do not access information stored on your computer or any Personal Data (e.g. name, address, email address or telephone number). Most web browsers automatically accept cookies, but you can choose to reject cookies by changing your browser settings. This may, however, prevent you from taking full advantage of our website.
+            </p>
+            <br />
+            <p>
+                Our website uses cookies to analyse website traffic, provide social media sharing, and preferring functionality to help us provide a better website visitor experience. In addition, cookies and pixels may be used to serve relevant ads to website visitors through third-party services such as Google Adwords and Facebook Adverts. These ads may appear on this website or other websites you visit.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                SHARING YOUR INFORMATION WITH THIRD PARTIES
+            </h5>
+            <br />
+            <p>
+                We do not and will not sell or deal Personal Data or any customer information.
+            </p>
+            <br />
+            <p>
+                Your Personal Data details are only disclosed to third-party suppliers when it is required by law, for goods or services which you have purchased, for payment processing or to protect our copyright, trademarks and other legal rights. To the extent that we do share your Personal Data with a service provider, we would only do so if that party has agreed to comply with our privacy standards as described in this privacy policy and in accordance with applicable law. Our contracts with third parties prohibit them from using any of your Personal Data for any purpose other than that for which it was shared.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                DISCLOSURE OF YOUR INFORMATION
+            </h5>
+            <br />
+            <p>
+                We may from time to time need to disclose certain information, which may include your Personal Data, to comply with a legal requirement, such as a law, regulation, court order, subpoena, warrant, in the course of a legal proceeding or in response to a law enforcement agency request. We may also use your Personal Data to protect the rights, property or safety of Conva-messenger.com, our customers or third parties.
+            </p>
+            <br />
+            <p>
+                If there is a change of control in one of our businesses (whether by merger, sale, transfer of assets or otherwise), customer information, which may include your Personal Data, could be transferred to a purchaser under a confidentiality agreement. We would only disclose your Personal Data in good faith and when required by any of the above circumstances.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                LINKS TO OTHER WEBSITES
+            </h5>
+            <br />
+            <p>
+                This website may contain links to other websites. These links are meant for your convenience only. Links to third-party websites do not constitute sponsorship, endorsement or approval of these websites. Please be aware that we are not responsible for the privacy practices of such other websites. We encourage our users to be aware, when they leave our website, to read the privacy statements of each and every website that collects personally identifiable information. This privacy policy applies solely to information collected by this website.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                CHANGE IN PRIVACY POLICY
+            </h5>
+            <br />
+            <p>
+                As we plan to ensure our privacy policy remains current, this policy is subject to change. We may modify this policy at any time, at our sole discretion and all modifications will be effective immediately upon our posting of the modifications on this website. Please return periodically to review our privacy policy.
+            </p>
+            <br />
+            <h5 className="uppercase font-semibold text-gray-700">
+                CONTACT US
+            </h5>
+            <br />
+            <p>
+                If you have any questions or concerns at any time about our privacy policy or the use of your Personal Data, please contact us at support@clinica-technologies.com and we will respond within 48 hours.
+            </p>
+            <br />
+        </div>
+    </div>
+}
