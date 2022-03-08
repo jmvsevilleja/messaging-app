e3b147d8220c7c4e67970237ca3e5347da9984c4
Jess Mark Sevilleja <jess@clinica.dev>
2022-03-08 13:43:12 +0800

    added clinica description responsive

--- src/Gmail/EmailSidebar.js
+++ src/Gmail/EmailSidebar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Messages from "./Messages";
 import GoogleButton from 'react-google-button'
 import Nav from "../components/Nav";
+import ConvoLogo from '../logo.svg';
 
 function EmailSidebar({
     isSigned,
@@ -22,7 +23,7 @@ function EmailSidebar({
         >
             <div className="flex justify-between item-center p-5 py-5">
                 <div className="flex items-center" >
-                    <div className=" font-bold text-gray-600 dark:text-white">Email</div>
+                    <div className=" font-bold text-gray-600 dark:text-white">Gmail</div>
                 </div>
                 {isSigned && <button type="button"
                     onClick={handleGoogleSignOut}
@@ -34,13 +35,25 @@ function EmailSidebar({
             </div>
             < div className="px-5"><Nav /></div>
             {!isSigned && <div className="flex flex-col items-center justify-center h-[calc(100vh-130px)]">
+                <div className="my-10">
+                    <img className="w-60 mx-5" src={ConvoLogo} alt="Conva" />
+                </div>
+                <div className="text-gray-600 dark:text-slate-400">
+                    Welcome to Conva!
+                </div>
+                <div className="m-5 text-sm text-gray-600 dark:text-slate-400 text-center">
+                    Access your accounts anytime, anywhere! Monitor your emails by reading, replying, and communicate with multimedia content.
+                    <br /><br />With convenience and efficiency, experience the latest features of managing your emails on the go.
+                </div>
                 <GoogleButton
                     onClick={handleGoogleSignInButton}
                 />
-                <div className="m-5 text-base text-gray-600 dark:text-slate-400 text-center">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dignissim fermentum dolor, vitae elementum eros mattis ac. Pellentesque semper facilisis libero sed finibus. Nulla facilisi. Mauris convallis interdum ante, vel ornare tellus sollicitudin.
+                <div className="m-5 text-sm font-base text-black dark:text-slate-400">
+                    Read our&nbsp;
+                    <a className="mt-5 text-primary hover:text-secondary" href="/privacy-policy">Privacy policy</a>
+                    &nbsp;and the&nbsp;
+                    <a className="mt-5 text-primary hover:text-secondary" href="/terms-and-condition">Terms of service</a>
                 </div>
-                <a className="text-base text-gray-500 hover:text-gray-600" href="/privacy-policy">Privacy policy</a>
             </div>}
             {isSigned && isLoading && <div className="flex items-center justify-center h-[calc(100vh-130px)]">
                 <div className="text-gray-600 dark:text-slate-400 opacity-50">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,31 +14,37 @@ export default function Nav() {
   return (
     <>
       <div className="justify-between item-center mb-5">
-        <div className="justify-left items-center text-sm w-full grid grid-cols-2 xs:grid-cols-4" >
+        <div className="justify-left items-center text-sm w-full grid grid-cols-5 xs:grid-cols-5 gap-1" >
           <button type="button"
             onClick={() => {
               handleNav("/chat")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 mr-4 py-1 my-1 " + (selected === "/chat" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/chat" ? "dark:bg-primary bg-primary text-white" : "")}
           >Chat</button>
           <button type="button"
             onClick={() => {
-              handleNav("/google")
+              handleNav("/gmail")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 mr-4 py-1 my-1 " + (selected === "/google" ? "dark:bg-primary bg-primary text-white" : "")}
-          >Google</button>
+            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/gmail" ? "dark:bg-primary bg-primary text-white" : "")}
+          >Gmail</button>
           <button type="button"
             onClick={() => {
               handleNav("/outlook")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 mr-4 py-1 my-1 " + (selected === "/outlook" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/outlook" ? "dark:bg-primary bg-primary text-white" : "")}
           >Outlook</button>
           <button type="button"
             onClick={() => {
               handleNav("/icloud")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 mr-4 py-1 my-1 " + (selected === "/icloud" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/icloud" ? "dark:bg-primary bg-primary text-white" : "")}
           >iCloud</button>
+          <button type="button"
+            onClick={() => {
+              handleNav("/clinica")
+            }}
+            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/clinica" ? "dark:bg-primary bg-primary text-white" : "")}
+          >Clinica</button>
         </div>
       </div>
     </>
