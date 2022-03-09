9f848786c7151f4fab0f92a45d9068a1fbc89f48
Jess Mark Sevilleja <jess@clinica.dev>
2022-03-09 18:42:21 +0800

    fixed nav style ang google loading

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,31 +19,31 @@ export default function Nav() {
             onClick={() => {
               handleNav("/chat")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/chat" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded py-1 " + (selected === "/chat" ? "dark:bg-primary bg-primary text-white" : "")}
           >Chat</button>
           <button type="button"
             onClick={() => {
               handleNav("/gmail")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/gmail" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded py-1 " + (selected === "/gmail" ? "dark:bg-primary bg-primary text-white" : "")}
           >Gmail</button>
           <button type="button"
             onClick={() => {
               handleNav("/outlook")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/outlook" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded py-1 " + (selected === "/outlook" ? "dark:bg-primary bg-primary text-white" : "")}
           >Outlook</button>
           <button type="button"
             onClick={() => {
               handleNav("/icloud")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/icloud" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded py-1 " + (selected === "/icloud" ? "dark:bg-primary bg-primary text-white" : "")}
           >iCloud</button>
           <button type="button"
             onClick={() => {
               handleNav("/clinica")
             }}
-            className={"dark:bg-white shadow-md rounded px-2 py-1 " + (selected === "/clinica" ? "dark:bg-primary bg-primary text-white" : "")}
+            className={"dark:bg-white shadow-md rounded py-1 " + (selected === "/clinica" ? "dark:bg-primary bg-primary text-white" : "")}
           >Clinica</button>
         </div>
       </div>
